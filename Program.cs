cd74e2f512faeb7507f89579368689e47e7ce77e
wellingtonfcdev <wellingtonfc@hotmail.com>
2024-04-15 13:52:57 -0300

    Implementando classe private OO

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿using OrientacaoOO.Models;
 
-Pessoa p1 = new Pessoa();
-p1.Nome = "Wellington";
-p1.Idade = 39;
-p1.Apresentar();
+ContaCorrente c1 = new ContaCorrente(123,1000);
+
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Wellington";
+// p1.Idade = 39;
+// p1.Apresentar();
